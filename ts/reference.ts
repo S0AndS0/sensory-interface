1e5c83d1c2c03c81f3f36409189e12aa756c2527
S0AndS0 <strangerthanbland@gmail.com>
2020-01-23 18:57:34 -0800

    :coffee: Adds TypeScript hints for JQuery

---
+++ ts/reference.ts
@@ -0,0 +1,2 @@
+/// <reference path="../node_modules/@types/jquery/index.d.ts"/>
+// Thanks for above be to [How to reference jquery in a typescript file...](https://stackoverflow.com/questions/42149342)
